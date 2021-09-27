b6470bd917260e265a1944ebf049a50bf5a37297
uai <noema01@outlook.jp>
2021-09-27 20:43:28 +0900

    再利用性を考慮したリファクタリング

--- src/StarRating.js
+++ src/StarRating.js
@@ -2,10 +2,10 @@ import React from "react";
 import { useState } from "react";
 import Star from "./Star";
 
-export default function StarRating({totalStars = 5}) {
+export default function StarRating({style = {}, totalStars = 5, ...props}) {
   const [ selectedStars, setSelectedStars ] = useState(0);
   return (
-    <>
+    <div style={{padding: "5px", ...style}} {...props}>
       {[...Array(totalStars)].map((n, i) => (
         <Star
           key={i}
@@ -17,6 +17,6 @@ export default function StarRating({totalStars = 5}) {
       <p>
         {selectedStars} of {totalStars} stars
       </p>
-    </>
+    </div>
   );
 }
