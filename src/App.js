186748e9048e649f979829377559fb1e1875773c
uai <noema01@outlook.jp>
2021-09-27 22:12:59 +0900

    レーティング機能の実装

--- src/App.js
+++ src/App.js
@@ -5,10 +5,23 @@ import './App.css';
 import ColorList from './ColorList';
 
 
-
 function App() {
-  const [colors] = useState(colorData);
-  return <ColorList colors={colors} />
+  const [colors, setColors] = useState(colorData);
+  return (
+    <ColorList
+      colors={colors}
+      onRateColor={(id, rating) => {
+        const newColors = colors.map(color =>
+          color.id === id ? { ...color, rating } : color
+        );
+        setColors(newColors)
+      }}
+      onRemoveColor={id => {
+        const newColors = colors.filter(color => color.id !== id);
+        setColors(newColors);
+      }}
+    />
+  )
 }
 
 export default App;
